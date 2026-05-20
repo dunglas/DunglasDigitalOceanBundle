ca834e18682c24ea2403bc58322d0129eee65523
Kévin Dunglas <kevin@dunglas.fr>
2026-05-20 13:21:41 +0200

    chore: allow Symfony 8 and modernize CI

--- tests/DependencyInjection/ConfigurationTest.php
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace Dunglas\DigitalOceanBundle\Tests\DependencyInjection;
 
 use Dunglas\DigitalOceanBundle\DependencyInjection\Configuration;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Config\Definition\Processor;
 
@@ -22,16 +23,14 @@ use Symfony\Component\Config\Definition\Processor;
  */
 class ConfigurationTest extends TestCase
 {
-    /**
-     * @dataProvider configsProvider
-     */
+    #[DataProvider('configsProvider')]
     public function testConfig(array $configs): void
     {
         $config = (new Processor())->processConfiguration(new Configuration(), $configs);
         $this->assertSame('foo', $config['connections']['default']['token']);
     }
 
-    public function configsProvider(): iterable
+    public static function configsProvider(): iterable
     {
         yield [
             [
